5cf1fbbeecb09e2ccd146232076c46e7fde9f3d0
Najib Sauqi R <97015365+Njsa13@users.noreply.github.com>
2022-06-17 18:39:22 +0700

    Menambahkan File/Folder

---
+++ tugas polymerism/tugas polymerism/PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_polymerism
+{
+    public class PrinterWindows
+    {
+        public virtual void Show()
+        {
+
+        }
+        public virtual void Print()
+        {
+
+        }
+    }
+}
